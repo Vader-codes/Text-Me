880f7bcb1d966425d3c58187e4ec0230b9eb0df5
vader-codes <bsr29002@gmail.com>
2026-07-08 17:27:55 +0530

    server files got changed

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -3,6 +3,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
 
 dotenv.config();
 
