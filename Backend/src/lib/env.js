65fed812e7feb18074043b1c85c75a70d0234554
vader-codes <bsr29002@gmail.com>
2026-07-10 14:47:15 +0530

    Update env.js

--- Backend/src/lib/env.js
+++ Backend/src/lib/env.js
@@ -1,12 +1,12 @@
 import "dotenv/config";
 
 export const ENV = {
-  PORT: Proces.env.PORT,
-  MONGO_URI: Proces.env.MONGO_URI,
-  JWT_SECRET: Proces.env.JWT_SECRET,
-  NODE_ENV: Proces.env.NODE_ENV,
-  CLIENT_URL: Proces.env.CLIENT_URL,
-  RESEND_API_KEY: Proces.env.RESEND_API_KEY,
-  EMAIL_FROM: Proces.env.EMAIL_FROM,
-  EMAIL_FROM_NAME: Proces.env.EMAIL_FROM_NAME,
+  PORT: Process.env.PORT,
+  MONGO_URI: Process.env.MONGO_URI,
+  JWT_SECRET: Process.env.JWT_SECRET,
+  NODE_ENV: Process.env.NODE_ENV,
+  CLIENT_URL: Process.env.CLIENT_URL,
+  RESEND_API_KEY: Process.env.RESEND_API_KEY,
+  EMAIL_FROM: Process.env.EMAIL_FROM,
+  EMAIL_FROM_NAME: Process.env.EMAIL_FROM_NAME,
 };
