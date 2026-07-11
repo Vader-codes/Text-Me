f51093d22debd849e7d71c243aca258584800869
vader-codes <bsr29002@gmail.com>
2026-07-11 19:19:19 +0530

    made message endpoints

--- Backend/src/routes/message.route.js
+++ Backend/src/routes/message.route.js
@@ -1,9 +1,13 @@
 import express from "express";
+import { getAllContacts } from "../controllers/messsage.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
+impor;
 
 const router = express.Router();
 
-router.get("/send", (req, res) => {
-  res.send("Send message endpoint");
-});
+router.get("/contacts", protectRoute, getAllContacts);
+router.get("/chats", protectRoute, getChatPartners);
+router.get("/:id", protectRoute, getMessagesByUserId);
+router.post("/send/:id", protectRoute, sendMessage);
 
 export default router;
