19a4839dd7e4fc815320d9332b128f431c1012ed
vader-codes <bsr29002@gmail.com>
2026-07-15 17:09:16 +0530

    some bugs in auth.middleware.js

--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -5,22 +5,22 @@ import { ENV } from "../lib/env.js";
 export const protectRoute = async (req, res, next) => {
   try {
     const token = req.cookies.jwt;
-    if (!token) {
-      return req
+    if (!token)
+      return res
         .status(401)
         .json({ message: "Unauthorized - No token provided" });
-    }
+
     const decoded = jwt.verify(token, ENV.JWT_SECRET);
     if (!decoded)
-      return req.status(401).json({ message: "Unauthorized - Invalid Token" });
+      return res.status(401).json({ message: "Unauthorized - Invalid token" });
 
     const user = await User.findById(decoded.userId).select("-password");
-    if (!user) return req.status(404).json({ message: "User not found" });
+    if (!user) return res.status(404).json({ message: "User not found" });
 
     req.user = user;
     next();
   } catch (error) {
-    console.log("Error in protectRoute Middleware:", error);
-    req.status(500).json({ message: "Internal server error" });
+    console.log("Error in protectRoute middleware:", error);
+    res.status(500).json({ message: "Internal server error" });
   }
 };
