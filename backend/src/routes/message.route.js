13bbe5d21c0c04ff44146103a022ad35e7bb70a2
vader-codes <bsr29002@gmail.com>
2026-07-15 17:11:56 +0530

    fixed some more bugs

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,14 +1,17 @@
 import express from "express";
-import { getAllContacts } from "../controllers/message.controller.js";
+import {
+  getAllContacts,
+  getChatPartners,
+  getMessagesByUserId,
+  sendMessage,
+} from "../controllers/message.controller.js";
 import { protectRoute } from "../middleware/auth.middleware.js";
-import { getChatPartners } from "../controllers/message.controller.js";
-import { getMessagesByUserId } from "../controllers/message.controller.js";
-import { sendMessage } from "../controllers/message.controller.js";
 import { arcjetProtection } from "../middleware/arcjet.middleware.js";
 
 const router = express.Router();
 
-// the middleware execute in order -so requests get rate-limited first, then authenticated.
+// the middlewares execute in order - so requests get rate-limited first, then authenticated.
+// this is actually more efficient since unauthenticated requests get blocked by rate limiting before hitting the auth middleware.
 router.use(arcjetProtection, protectRoute);
 
 router.get("/contacts", getAllContacts);
