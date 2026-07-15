8e62dc59ded0a795927c1c6764aa6798cd07a391
vader-codes <bsr29002@gmail.com>
2026-07-15 17:11:14 +0530

    some changes to auth.route.js

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -5,9 +5,9 @@ import {
   logout,
   updateProfile,
 } from "../controllers/auth.controller.js";
-
 import { protectRoute } from "../middleware/auth.middleware.js";
 import { arcjetProtection } from "../middleware/arcjet.middleware.js";
+
 const router = express.Router();
 
 router.use(arcjetProtection);
@@ -16,7 +16,7 @@ router.post("/signup", signup);
 router.post("/login", login);
 router.post("/logout", logout);
 
-router.put("/update-profile ", protectRoute, updateProfile);
+router.put("/update-profile", protectRoute, updateProfile);
 
 router.get("/check", protectRoute, (req, res) =>
   res.status(200).json(req.user),
